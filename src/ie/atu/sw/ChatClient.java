c20e0eeb7a6ab645e0b63241dd1974888b667ce9
sttadic <stjepan.tadich@gmail.com>
2025-01-04 16:03:03 +0000

    Implement very basic functionality for client and server.

--- src/ie/atu/sw/ChatClient.java
+++ src/ie/atu/sw/ChatClient.java
@@ -1,5 +1,33 @@
 package ie.atu.sw;
 
-public class ChatClient {
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 
+import static java.lang.System.out;
+
+public class ChatClient {
+	private int port;
+	private String hostname = "localhost";
+	
+	public void startClient() {
+		
+		try (Socket socket = new Socket(hostname, port)) {
+			out.println("Connected to server on host: " + hostname);
+			
+			socket.setSoTimeout(5000);
+			
+			InputStream input = socket.getInputStream();
+			byte[] inputBytes = input.readAllBytes();
+			String message = new String(inputBytes, StandardCharsets.US_ASCII);
+			out.println(message);
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/ie/atu/sw/ChatServer.java
+++ src/ie/atu/sw/ChatServer.java
@@ -1,5 +1,28 @@
 package ie.atu.sw;
 
-public class ChatServer {
+import java.io.*;
+import java.net.*;
+import static java.lang.System.out;
 
+public class ChatServer {
+	private final static int PORT = 13;
+	
+	public void startServer() {
+		try (ServerSocket server = new ServerSocket(PORT)) {
+			 
+			while (true) {
+				out.println("ChatServer listening on port " + PORT);
+				
+				try (Socket connection = server.accept()) {
+					out.println("Client connected from host " + connection.getInetAddress() + ", port: " + connection.getPort());
+					
+					Writer output = new OutputStreamWriter(connection.getOutputStream());
+					output.write("Hello World \r\n");
+					output.flush();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
