0c89c7b367497b91ee3796a56c234796e66da4fe
sttadic <stjepan.tadich@gmail.com>
2025-01-05 18:32:15 +0000

    Delegate exception handling to a dedicated method. Change some variable names.

--- src/ie/atu/sw/ChatClient.java
+++ src/ie/atu/sw/ChatClient.java
@@ -11,62 +11,67 @@ public class ChatClient {
 	private int port = 13;
 	private String hostname = "localhost";
 	private final Scanner scan;
-	
+
 	public ChatClient() {
 		this.scan = new Scanner(System.in);
 		this.userName = setName();
 	}
-	
+
 	private String setName() {
 		String uName = "";
 		while (true) {
-			out.println("Please enter your name: ");
+			out.print("Please enter your name: ");
 			uName = scan.nextLine();
 			if (uName.isEmpty()) {
-				out.println("Name cannot be empty! Please try again");
+				out.println("Name field cannot be empty!");
 				continue;
 			}
 			break;
 		}
 		return uName;
 	}
-	
+
 	public void startClient() {
-		
 		try (Socket socket = new Socket(hostname, port);
 			// Setup input and output streams
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
-			
-			out.println("Connected to server on host: " + hostname);
+
 			// Read welcome message from the server
 			String receivedMsg = reader.readLine();
 			out.println("Server: " + receivedMsg);
-			
+
 			// Send messages to the server
 			String sentMsg;
 			while (true) {
 				out.print("Message: ");
 				sentMsg = scan.nextLine();
 				writer.println(sentMsg);
-			
-			// Read and print the server's response
-			receivedMsg = reader.readLine();
-			out.println("Server: " + receivedMsg);
-			
+
+				// Read and print the server's response
+				receivedMsg = reader.readLine();
+				out.println("Server: " + receivedMsg);
+
 			}
-			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		} catch (Exception e) {
+			exceptionHandler(e);
 		} finally {
 			scan.close();
 		}
 	}
-	
-	
+
+	private void exceptionHandler(Exception e) {
+		if (e instanceof ConnectException) {
+			out.println("\r\nNo ChatServer on host '" + hostname + "', port " + port + ". Please try again later.\r\n");
+		} else if (e instanceof SocketException) {
+			out.println("\r\nChatServer encountered an error. Chat session terminated!\r\n");
+		} else {
+			out.println("Something went wrong. Chat session terminated.\n\r");
+		}
+	}
+
 	public static void main(String[] args) {
-		new ChatClient();
+		new ChatClient().startClient();
 	}
 }
--- src/ie/atu/sw/ChatServer.java
+++ src/ie/atu/sw/ChatServer.java
@@ -16,19 +16,19 @@ public class ChatServer {
 				
 				try (Socket connection = server.accept();
 					// Setup input and output stream
-					BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-					PrintWriter output = new PrintWriter(connection.getOutputStream(), true)) { // autoFlush 
+					BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					PrintWriter writer = new PrintWriter(connection.getOutputStream(), true)) { // autoFlush 
 					
 					out.println("Client connected from host " + connection.getInetAddress() + ", port: " + connection.getPort());
 					// Send welcome message to the client
-					output.println("Hello from the ChatServer!");
+					writer.println("Hello from the ChatServer!");
 					
 					// Continously read messages from the client
 					String receivedMsg;
-					while ((receivedMsg = input.readLine()) != null) {
+					while ((receivedMsg = reader.readLine()) != null) {
 						out.println("Received: " + receivedMsg);
 						// Optionally send response back
-						output.println("Message received: " + receivedMsg);
+						writer.println("Message received: " + receivedMsg);
 					}
 				}
 			}
@@ -38,6 +38,6 @@ public class ChatServer {
 	}
 	
 	public static void main(String[] args) {
-		new ChatServer().startServer();;
+		new ChatServer().startServer();
 	}
 }
