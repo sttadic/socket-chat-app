21ab26b17262e7c8c0b07a940171b2e089a96b28
sttadic <stjepan.tadich@gmail.com>
2025-01-06 12:05:27 +0000

    Remove try-with-resources block for executor from ChatServer to gain more control over its lifecycle.

--- src/ie/atu/sw/ChatServer.java
+++ src/ie/atu/sw/ChatServer.java
@@ -2,34 +2,37 @@ package ie.atu.sw;
 
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import static java.lang.System.out;
 
 public class ChatServer {
 	private final static int PORT = 13;
 	private final ClientManager clientManager;
+	private final ExecutorService executor;
 
 	public ChatServer() {
 		this.clientManager = new ClientManager();
+		this.executor = Executors.newVirtualThreadPerTaskExecutor();
 	}
 
 	public void startServer() {
 		try (ServerSocket server = new ServerSocket(PORT)) {
 			out.println("ChatServer started on port: " + PORT);
 
-			try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-				while (true) {
-					Socket clientConnection = server.accept();
-					out.println("Client connected from host " + clientConnection.getInetAddress() + ", port: "
-							+ clientConnection.getPort());
+			while (true) {
+				Socket clientConnection = server.accept();
+				out.println("Client connected from host " + clientConnection.getInetAddress() + ", port: "
+						+ clientConnection.getPort());
 
-					// Handle client connection in a new thread
-					executor.execute(() -> handleClient(clientConnection));
-				}
+				// Handle client connection in a separate thread
+				executor.execute(() -> handleClient(clientConnection));
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			shutdownExecutor();
 		}
 	}
 
@@ -37,14 +40,14 @@ public class ChatServer {
 		try (var reader = new BufferedReader(new InputStreamReader(clientConnection.getInputStream()));
 				var writer = new PrintWriter(clientConnection.getOutputStream(), true)) {
 			String clientName = reader.readLine();
-			
+
 			// Add client to the client manager
 			var connection = new Connection(clientConnection, reader, writer, clientName);
 			clientManager.addClient(connection);
-			
+
 			// Notify clients about new user
 			clientManager.broadcastMsg("SERVER: " + clientName + " has joined the chat", null);
-			
+
 			// Listen for client messages
 			String message;
 			while ((message = reader.readLine()) != null) {
@@ -54,13 +57,23 @@ public class ChatServer {
 				}
 				clientManager.broadcastMsg(message, connection);
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	private void shutdownExecutor() {
+		executor.shutdown();
+		try {
+			if (!executor.awaitTermination(10, TimeUnit.SECONDS)) {
+				executor.shutdownNow();
+			}
+		} catch (InterruptedException e) {
+			executor.shutdownNow();
+		}
+	}
+
 	public static void main(String[] args) {
 		new ChatServer().startServer();
 	}
