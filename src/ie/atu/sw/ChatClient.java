13efc214c772dc8ab22caecc3bb9d5f1aef76031
sttadic <stjepan.tadich@gmail.com>
2025-01-06 23:46:14 +0000

    Change handling of some exceptions. Resources closed after exception is cought and error displayed. Running atomic boolean set to false if any exception occurs.

--- src/ie/atu/sw/ChatClient.java
+++ src/ie/atu/sw/ChatClient.java
@@ -34,6 +34,7 @@ public class ChatClient {
 			this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			this.writer = new PrintWriter(socket.getOutputStream(), true);
 		} catch (IOException e) {
+			running.set(false);
 			exceptionHandler(e);
 		}
 	}
@@ -71,15 +72,20 @@ public class ChatClient {
 				String inMessage = reader.readLine();
 				if (inMessage == null) {
 					running.set(false); // Connection is closed
+					closeResources();
 					break;
 				}
 				out.println(inMessage);
 			}
 		} catch (Exception e) {
-			if (running.get()) exceptionHandler(e);
+			if (running.get()) {
+				running.set(false); 
+				exceptionHandler(e);
+			}
 		}
 	}
 
+	// Prompt for a name. Reprompt for empty string
 	private String setName() {
 		String userName;
 		while (true) {
@@ -94,16 +100,17 @@ public class ChatClient {
 		return userName;
 	}
 
+	// Handle exception and close resources
 	private void exceptionHandler(Exception e) {
-		System.out.println(e.getClass());
-		closeResources();
 		if (e instanceof ConnectException) {
-			out.println("\r\nNo ChatServer is found. Please try again later.\r\n");
+			out.println("\r\nChatServer cannot be found: " + e.getMessage() + "\r\n");
 		} else if (e instanceof SocketException) {
-			out.println("\r\nChat session terminated due to an error!\r\n");
+			out.println("\r\nChat session terminated due to an error: " + e.getMessage() + "\r\n");
 		} else {
-			out.println("Something went wrong. Chat session terminated.\n\r");
+			out.println("\r\nSomething went wrong: " + e.getMessage() + "\n\r");
+			e.printStackTrace();
 		}
+		closeResources();
 	}
 
 	// Synchronized access to close resources method
@@ -131,19 +138,20 @@ public class ChatClient {
 	 * @param args optional arguments for hostname and port
 	 * @throws ConnectException if specified port is unavailable
 	 * @throws UnknownHostException if the specified host is unknown
-	 * @throws NoSuchElementException if session is abrubted forcefully (ctrl+c)
+	 * @throws Exception if error occures for any other reason
 	 */
-	public static void main(String[] args) throws UnknownHostException, IOException {
+	public static void main(String[] args) {
 		// Default configuration
 		String host = "localhost";
 		int port = ChatServer.PORT;
 		
-		// Show usage info for too many arguments
+		// Show usage info in case of too many arguments
 		if (args.length > 2) {
 			out.println("\r\nUsage: java ChatClient [host] [port]");
 			out.println("With no arguments -> host: " + host + ", port: " + port + "\r\n");
 			return;
 		}
+		
 		// Read command line arguments and assign to appropriate variables
 		if (args.length > 0) host = args[0];
 		if (args.length > 1) port = Integer.parseInt(args[1]);
@@ -163,8 +171,9 @@ public class ChatClient {
 			out.println("\r\nCould not connect to '" + host + "' at port " + port + "\r\n");
 		} catch (UnknownHostException e) {
 			out.println("\r\nCould not connect to hostname: " + host + "\r\n");
-		} catch (NoSuchElementException e) {
-			out.println("\r\nChat session terminated!\r\n");
+		} catch (Exception e) {
+			out.println("\r\nError: " + e.getMessage() + "\r\n");
+			e.printStackTrace();
 		}
 	}
 }
